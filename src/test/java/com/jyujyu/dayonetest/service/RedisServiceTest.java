881615454bbd0de375fbe140a790738291b4606f
yhlee <xxxoxxo002@gmail.com>
2024-03-25 01:21:50 +0900

    Redis IntegrationTest 작성

---
+++ src/test/java/com/jyujyu/dayonetest/service/RedisServiceTest.java
@@ -0,0 +1,30 @@
+package com.jyujyu.dayonetest.service;
+
+import com.jyujyu.dayonetest.IntegrationTest;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class RedisServiceTest extends IntegrationTest {
+
+    @Autowired
+    private RedisService redisService;
+
+    @Test
+    @DisplayName("Redis Get/Set 테스트")
+    public void redisGetSetTest() {
+        // given
+        String expectedValue = "hello";
+        String key = "hi";
+
+        // when
+        redisService.set(key, expectedValue);
+
+        // then
+        String actualValue = redisService.get(key);
+        Assertions.assertEquals(expectedValue, actualValue);
+
+
+    }
+}
